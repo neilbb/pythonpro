9032acebe872dbd7550b1067e10cda13a5ceb02a
Neil <neil.budnarain@hotmail.com>
2017-10-17 17:38:07 -0400

    update

--- regex/problems.py
+++ regex/problems.py
@@ -5,22 +5,27 @@
 import re
 
 if __name__ == '__main__':
-    f = open("sample.txt" ,encoding='utf-8',mode='r')
+    f = open("names.txt" ,encoding='utf-8',mode='r')
     #for i in f:
     #   print(i)
 
     a = f.read()
 
+    f.close()
 
 
-    print(re.match(r"Lore",a))
-    print(50*'*')
-    print(re.search(r"ore",a))
 
+
+   # print(re.match(r"Lore",a))
+   # print(50*'*')
+   # print(re.search(r"ore",a))
+    #print(80*'*')
+
+    print(re.findall(r'\(?\d+\)?-?\s?\d+-\d+',a))
 
 
     #print(a)
-    f.close()
+
 
 
 
