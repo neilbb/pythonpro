0d465e238e9a4742db99c2d83c4a8f85a7830286
Neil <neil.budnarain@hotmail.com>
2017-10-18 02:09:33 -0400

    updated

--- database/problem.py
+++ database/problem.py
@@ -17,10 +17,50 @@ class Student(Model):
 		database = db
 
 
+s = [
+	{'user':'johndoe',
+	'lvl':3000
+	},{'user':'jaydoe',
+	'lvl':321999
+	},
+	{'user':'ramdoe',
+	'lvl':30434
+	},
+	{'user':'chaitoedoe',
+	'lvl':5555
+	},
+	{'user':'laladoe',
+	'lvl':302
+	},
+
+]
+
+
+def add_students():
+
+	for i in s:
+		try:
+			Student.create(username=i['user'],
+						points=i['lvl'])
+		except IntegrityError:
+			student_record = Student.get(username=i['user'])
+			student_record.points = i['lvl']
+			student_record.save()
+
+
+def top_student():
+	#student = Student.select() # get all student records in the database
+	student = Student.select().order_by(Student.points.desc()).get() # there is a ascending too and get() will get the first one record
+	return student
+
 
 if __name__ == '__main__':
 	db.connect()
 	db.create_tables([Student],safe=True)
+	add_students()
+	a = top_student()
+	print("the top student",a.username,a.points)
+
 
 
 
