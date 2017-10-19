1eb592487ccc6f3f90d93eccc84e359ecf3bfe2d
Neil <neil.budnarain@hotmail.com>
2017-10-19 00:37:16 -0400

    update

--- database/ordereddicttest.py
+++ database/ordereddicttest.py
@@ -6,10 +6,10 @@ import ordereddict
 
 class DictTests(unittest.TestCase):
 	def test_one_plus_five(self):
-		assert 1 + 5 == 6
+		assert (1 + 5) == 6
 
 	def test_one_plus_one(self):
-		assert not 1 + 1 == 5
+		assert not (1 + 1) == 5
 
 
 
